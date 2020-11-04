63d7be916b354826befcfee0f170e81f39315f42
mauraqp <mauraqoonitah@gmail.com>
2020-11-05 03:27:30 +0700

    innitial commit

---
+++ src/scripts/utils/drawer-initiator.js
@@ -0,0 +1,26 @@
+
+// logika agar drawer dapat berfungsi semestinya.
+const DrawerInitiator = {
+    init({ button, drawer, content }) {
+      button.addEventListener('click', (event) => {
+        this._toggleDrawer(event, drawer);
+      });
+   
+      content.addEventListener('click', (event) => {
+        this._closeDrawer(event, drawer);
+      });
+    },
+   
+    _toggleDrawer(event, drawer) {
+      event.stopPropagation();
+      drawer.classList.toggle('open');
+    },
+   
+    _closeDrawer(event, drawer) {
+      event.stopPropagation();
+      drawer.classList.remove('open');
+    },
+  };
+   
+   
+  export default DrawerInitiator;
