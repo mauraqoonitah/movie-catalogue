0733158eadf1a2d3c6bd02467d068813c8e95310
mauraqp <mauraqoonitah@gmail.com>
2020-11-18 17:46:23 +0700

    web socket untuk menampilkan live feeds dari movie

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,8 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
-
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
     button: document.querySelector('#hamburgerButton'),
@@ -18,9 +19,6 @@ window.addEventListener('hashchange', () => {
 // load (ketika halaman dimuat)
 window.addEventListener('load', () => {
     app.renderPage();
-});
-
-window.addEventListener('load', () => {
-    app.renderPage();
     swRegister();
+    WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
